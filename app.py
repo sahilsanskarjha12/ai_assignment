8b19422236ff6f5eeb64d0aceb8046ad9f525d70
sahilsanskarjha <sahilsanskarjha@outlook.com>
2023-11-26 14:27:02 +0530

    new2

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 import openai
 
 app = Flask(__name__)
 openai.api_key = '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'  # Replace with your OpenAI API key
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/ask', methods=['POST'])
 def ask():
     data = request.get_json()
     user_query = data.get('query')
+
+    # Print or log the user query for debugging
+    print("User Query:", user_query)
 
     # Use OpenAI GPT to generate a response
     response = openai.Completion.create(
@@ -19,6 +18,10 @@ def ask():
         prompt=user_query,
         max_tokens=150
     )
+
+    # Print or log the OpenAI GPT response for debugging
+    print("OpenAI Response:", response)
+
     return jsonify({'response': response.choices[0].text})
 
 if __name__ == '__main__':
